c314a706e311a9fd8bc50d8e7165ad1e6444cb80
Nick Pennock <npennock@cisco.com>
2022-06-08 14:52:45 -0700

    Refactor generic oauth structure and documentation

--- src/generic-api-oauth.js
+++ src/generic-api-oauth.js
@@ -3,10 +3,18 @@ import assert from 'assert';
 import fetch from 'node-fetch';
 
 
-// This configuration object should drive the configuration of this transaction.
-// The url, method, headers, and body fields set the request to make.
-// The auth object controls the authentication details
-// The validation object controls validation of the request
+/**
+ * The configuration object defines the transaction to execute and how to validate the response
+ * 
+ * @param {String} url - The target URL of the transaction
+ * @param {String} method - The HTTP method
+ * @param {Object} headers - The headers represented as key/value pairs
+ * @param {String} body - The body of the request
+ * @param {String} marker - The TE marker name for measuring the transaction
+ * @param {String|Boolean} validation.statusCode - A comma separated list of ports or port ranges 
+ *                                                 considered valid, or Boolean true to use 200-399
+ * @param {String|RegExp} validation.content - A string or RegExp that must match the content of the reponse
+ */
 const configuration = {
     'url':' https://myapp.domain.tld',
     'method': 'POST',
@@ -14,36 +22,58 @@ const configuration = {
     'body': JSON.stringify({
         "jason":"content",
     }),
-    'auth': {
-        'url': 'https://oauth.provider.tld/token',
-        'username': 'myUser',
-        'credential': 'myCredentialName',
-        'method': 'POST'
-    },
+    'marker': 'HTTP-Transaction',
     'validation': {
         "statusCode": "200-299,302",
         "content": /^SUCCESS/
     },
 };
 
+/**
+ * This object drives the details of the authentication process.
+ * @param {String} url - The OAuth url
+ * @param {String} username - The username for authentication
+ * @param {String} credentialName - The ThousandEyes credential name containing the password for authentication
+ * @param {String} method - The HTTP method
+ */
+const auth = {
+    'url': 'https://oauth.provider.tld/token',
+    'username': 'myUser',
+    'credentialName': 'myCredentialName',
+    'method': 'POST'
+};
+
 execute();
 
 /**
- * Execute the transaction, first authenticating and generating an OAuth token,
- * then using this token, send the API request, and finally validate the 
- * response.
+ * Execute the transaction, first authenticating and generating an OAuth token, then using this
+ * token, send the API request
  */
 async function execute() {
+    const token = await authenticate(auth);
+    await executeRequest(configuration, token);
+}
 
-    // This transaction uses the client_credentials grant type.  You can modify
-    // this section to use a different grant type if necessary.
-    const url = configuration.auth.url;
+/**
+ * OAuth Authentication using client_credentials
+ * @param {Object.String} auth.url - The OAuth url
+ * @param {Object.String} auth.username - The username for authentication
+ * @param {Object.String} auth.credentialName - The ThousandEyes credential name containing the
+ *                                              password for authentication
+ * @param {Object.String} auth.method - The HTTP method
+ * @returns {String} - The generated OAuth token
+ */
+async function authenticate({url, username, credentialName, method}) {
+    /* 
+    * This transaction uses the client_credentials grant type.  You can modify this section to use 
+    * a different grant type if necessary.
+    */
     const body = 'grant_type=client_credentials'+
-                 '&client_id='+encodeURIComponent(configuration.auth.username)+
-                 '&client_secret='+encodeURIComponent(credentials.get(configuration.auth.credential));
+                 '&client_id='+encodeURIComponent(username)+
+                 '&client_secret='+encodeURIComponent(credentials.get(credentialName));
 
     const request = {
-        method: configuration.auth.method,
+        method,
         body,
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
@@ -56,69 +86,79 @@ async function execute() {
     markers.stop('OAuth');
 
     assert('access_token' in responseJSON, "No Oauth Token");
-    await transaction(responseJSON.access_token);
+    return responseJSON.access_token;
 }
 
 /**
  * Using the authentication token, execute the API transaction
+ * @param {String} config.url - The target URL of the transaction
+ * @param {String} config.method - The HTTP method
+ * @param {Object} config.headers - The headers represented as key/value pairs
+ * @param {String} config.body - The body of the request
+ * @param {String} config.marker - The TE marker name for measuring the transaction
+ * @param {String} config.validation.statusCode - A comma separated list of ports or port ranges
+ *                                                considered valid
+ * @param {String|RegExp} config.validation.content - A string or RegExp that must match the content
+ *                                                    of the reponse
  */
 
-async function transaction(tokenData){
+async function executeRequest({ headers, method, body, url, marker, validation }, token) {
 
-    if (! ('headers' in configuration)){
-        configuration.headers = {}
+    if (token) {
+        if (! (headers instanceof Object)) {
+            headers = {}
+        }
+        headers.Authorization = 'Bearer '+token;
     }
-    configuration.headers['Authorization'] = 'Bearer '+tokenData;
 
-    const requestBody = {
-        method: configuration.method,
-        body:    configuration.body,
-        headers: configuration.headers
-    };
-
-    markers.start('HTTP-Transaction');
-    const response = await fetch(configuration.url, requestBody);
+    markers.start(marker);
+    const response = await fetch(url, { method, body, headers });
     const responseText = await response.text();
-    markers.stop('HTTP-Transaction');
+    markers.stop(marker);
 
-    if ('validation' in configuration){
-        validateRequest(response, responseText, configuration.validation);
+    if (validation) {
+        validateRequest(response, responseText, validation);
     }
 }
 
 /**
  * Validate the response
+ * @param {Object} response - The fetch response object
+ * @param {String} responseText - The response formatted as a string
+ * @param {String|Boolean} validation.statusCode - A comma separated list of ports or port ranges 
+ *                                                 considered valid, or Boolean true to use 200-399
+ * @param {String|RegExp} validation.content - A string or RegExp that must match the content of the reponse
  */
-function validateRequest(response, responseText, toValidate) {
-
-    if ('statusCode' in toValidate){
+function validateRequest(response, responseText, {statusCode, content}) {
+    if (statusCode) {
         const status = parseInt(response.status)
 
-        if (toValidate.statusCode === true){
+        if (statusCode === true) {
             assert((status >= 200 && status < 400),
                 "HTTP Status Code NOT OK.");
 
-        } else if (toValidate.statusCode){
-            const ports = toValidate.statusCode.split(',');
+        } else if (statusCode) {
+            const ports = statusCode.split(',');
             assert(ports.some((port) => {
                 const parts = port.split("-");
-                if (parts.length == 1){
+                if (parts.length == 1) {
                     return (parseInt(port) == status);
-                } else if (parts.length == 2){
+                } else if (parts.length == 2) {
                     return (parseInt(parts[0]) <= status &&
                                 parseInt(parts[1]) >= status)
                 }
                 return false
-            }), `HTTP status code '${response.status}' does not match valid options: ${toValidate.statusCode}`);
+            }), `HTTP status code '${response.status}' does not match valid options: ${statusCode}`);
         }
     }
-    if ('content' in toValidate){
-        if (toValidate.content instanceof RegExp){
-            assert(toValidate.content.test(responseText),
-                "Response did not match regular expression: "+toValidate.content)
+
+    if (content) {
+        if (content instanceof RegExp){
+            assert(content.test(responseText),
+                "Response did not match regular expression: "+content)
         } else {
-            assert(responseText.includes(toValidate.content),
-                "Response did not contain: "+toValidate.content);
+            assert(responseText.includes(content),
+                "Response did not contain: "+content);
         }
     }
 }
